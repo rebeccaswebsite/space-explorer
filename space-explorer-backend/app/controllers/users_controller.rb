3a1f4761e6b1c0023b9f4d5ee65e77dc04d64b0f
0118ayush <ayush_gehlot@hotmail.com>
2019-05-28 14:57:58 +0100

    backend index

--- space-explorer-backend/app/controllers/users_controller.rb
+++ space-explorer-backend/app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    
+
 end
--- space-explorer-backend/db/seeds.rb
+++ space-explorer-backend/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+char1 = Character.create(name: "Mario", bio: "Whipped")
+char2 = Character.create(name: "Luigi", bio: "Plumber")
+char3 = Character.create(name: "Princess Peach", bio: "Spoilt richkid.")
+
+
+user1 = User.create(name: "Rebecca", character_id: char1.id)
+user2 = User.create(name: "Ash", character_id: char3.id)
+user3 = User.create(name: "Fran", character_id: char2.id)
+
+
+
+
+
+
