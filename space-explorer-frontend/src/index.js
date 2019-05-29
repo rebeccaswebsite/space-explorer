3039bd5343b79542043e959a5f90b94ce42dde35
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-29 11:45:02 +0100

    fetch methods

--- space-explorer-frontend/src/index.js
+++ space-explorer-frontend/src/index.js
@@ -1,23 +1,69 @@
-const charactersUrl = "http://localhost:3000/characters";
-const usersUrl = "http://localhost:3000/users/";
+const charactersUrl = "http://localhost:3000/characters"
+const usersUrl = "http://localhost:3000/users/"
 
 const getCharacters = () => {
   return fetch(charactersUrl)
     .then(response => response.json())
-    .then(json => console.log(json));
-};
+    .then(json => console.log(json))
+}
 
 const getCharacter = character => {
   return fetch(`${charactersUrl}/${character.id}`)
     .then(response => response.json())
-    .then(json => console.log(json));
-};
+    .then(json => console.log(json))
+}
 
 const getUsers = () => {
   return fetch(usersUrl)
     .then(resp => resp.json())
-    .then(object => console.log(object));
-};
+    .then(object => console.log(object))
+}
 
+const getUser = user => {
+  return fetch(`${usersUrl}/${user.id}`)
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
 
-cons
+const createUser = obj => {
+  // obj = {
+  //   name: formEl.name.value,
+  //   character_id: formEl.character.value.id
+  // }
+  return fetch(usersUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(obj)
+  })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+const updateUser = obj => {
+  // obj = {
+  //   name: formEl.name.value,
+  //   character_id: formEl.character.value.id
+  // }
+  return fetch(`${usersUrl}/${obj.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(obj)
+  })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+const deleteUser = obj => {
+  // obj = {
+  //  id: buttonEl.delete
+  // }
+  return fetch(`${usersUrl}/${obj.id}`, {
+    method: "DELETE"
+  })
+}
