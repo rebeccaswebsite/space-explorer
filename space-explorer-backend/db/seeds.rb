140ee2724253c9b8ba21d7ec0fc45e72d231f41c
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-05-30 18:16:16 +0100

    change seed data images

--- space-explorer-backend/db/seeds.rb
+++ space-explorer-backend/db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-char1 = Character.create(name: "Star Ship", bio: "Intergalactic space cruiser", image: "https://pngimg.com/uploads/starwars/starwars_PNG30.png")
-char2 = Character.create(name: "Starcraft", bio: "Icy blue like your soul", image: "https://pngimg.com/uploads/starcraft/starcraft_PNG7.png")
-char3 = Character.create(name: "Astronaut", bio: "The original space explorer", image: "https://pngimg.com/uploads/astronaut/astronaut_PNG37.png")
+char1 = Character.create(name: "Star Ship", bio: "Intergalactic space cruiser", image: "./public/starwars.png")
+char2 = Character.create(name: "Starcraft", bio: "Icy blue like your soul", image: "./public/starcraft.png")
+char3 = Character.create(name: "Astronaut", bio: "The original space explorer", image: "./public/astronaut.png")
  
 
 user1 = User.create(name: "Rebecca", character_id: char1.id)
